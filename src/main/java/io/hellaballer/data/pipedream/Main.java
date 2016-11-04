c922b927bf3a73f69171e35a8109c52e64301f82
kylevedder <kyle.c.vedder@gmail.com>
2016-11-04 18:00:52 -0400

    Added basic threader and workers

--- src/main/java/io/hellaballer/data/pipedream/Main.java
+++ src/main/java/io/hellaballer/data/pipedream/Main.java
@@ -1,7 +1,16 @@
 package io.hellaballer.data.pipedream;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
+	
+	ExecutorService executor = Executors.newFixedThreadPool(5);
+
+	
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		
+		
 	}
 }
---
+++ src/main/java/io/hellaballer/data/pipedream/WorkerThread.java
@@ -0,0 +1,16 @@
+package io.hellaballer.data.pipedream;
+
+public class WorkerThread implements Runnable {
+
+	// TODO Add higher order function.
+	private final int index;
+	public WorkerThread(int index) {
+		this.index = index;
+	}
+	
+	@Override
+	public void run() {
+		
+	}
+
+}
