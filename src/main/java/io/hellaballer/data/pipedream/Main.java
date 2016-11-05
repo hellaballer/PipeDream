63edc3a6a2178544a2dbc14d169c412da1972d34
Itamar Levy-Or <ilevyor@umass.edu>
2016-11-04 23:19:47 -0400

    added fileShard fn

--- src/main/java/io/hellaballer/data/pipedream/Main.java
+++ src/main/java/io/hellaballer/data/pipedream/Main.java
@@ -5,11 +5,24 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Function;
+import java.io.File;
 
 public class Main {
 
 	ExecutorService executor = Executors.newFixedThreadPool(5);
-
+	
+	static Function <String, List<File>> fileShard = e -> {
+		File[] files =  new File(e).listFiles();
+		ArrayList<File> fileList = new ArrayList<File>();
+		for (File f: files)
+			if(f.toString().endsWith(".mp4"))
+				fileList.add(f);
+		return fileList;	
+	};
+	
+	
+	
 	public static void main(String[] args) {
 		System.out.println("Starting...");
 
@@ -18,6 +31,7 @@ public class Main {
 		sharder.runShard(e -> Arrays.asList(e / 3, e / 3, e / 3));
 
 		List<Double> out = sharder.getOutputs();
+		
 		sharder.destroy();
 
 		System.out.println("Output: " + out);
@@ -41,6 +55,12 @@ public class Main {
 
 		System.out.println("FINAL VAL " + r.getOutput());
 		r.destory();
+		
+		Sharder<String, File> s = new Sharder<>("/Users/itamarlevy-or/fileShardTest");
+		s.runShard(fileShard);
+		List<File> fileOut = s.getOutputs();
+		for(File f: fileOut)
+			System.out.println(f);
 
 		// FFMPegWrapper.convertVideosToAudio(new
 		// File("/home/kyle/Documents/ObamaData/A_Bold_New_Course_for_NASA.mp4"));
